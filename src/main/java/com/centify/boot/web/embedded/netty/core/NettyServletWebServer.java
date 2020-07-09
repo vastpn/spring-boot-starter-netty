83f741eaccf5ddf730fcf6e8cab92b66a66e47cf
tanlin <sdrpfBZTG8HQ>
2020-07-09 16:01:47 +0800

    无线程block

--- src/main/java/com/centify/boot/web/embedded/netty/core/NettyServletWebServer.java
+++ src/main/java/com/centify/boot/web/embedded/netty/core/NettyServletWebServer.java
@@ -7,6 +7,7 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioChannelOption;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -127,6 +128,7 @@ public class NettyServletWebServer implements WebServer {
 //                    new DefaultThreadFactory("workerGroup"));
 //
 //        }else{
+//        workerGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2,new DefaultThreadFactory("workerGroup"));
         workerGroup =new NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2,
                 new ForkJoinPool(Runtime.getRuntime().availableProcessors() * 2));
 //        }
