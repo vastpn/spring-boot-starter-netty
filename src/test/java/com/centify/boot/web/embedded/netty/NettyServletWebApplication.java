602775ca62175ed861670f6b0417c945d72d5a80
tanlin <sdrpfBZTG8HQ>
2020-08-11 14:37:41 +0800

    修复 文件下载 测试案例：
    headers.add("Content-Disposition", String.format("attachment;filename=%s", new String(file.getFilename().getBytes("utf-8"), "ISO8859-1")));
    
    filename中不能加双引号，否则浏览器识别不了，名字被默认为download

--- src/test/java/com/centify/boot/web/embedded/netty/NettyServletWebApplication.java
+++ src/test/java/com/centify/boot/web/embedded/netty/NettyServletWebApplication.java
@@ -71,7 +71,7 @@ public class NettyServletWebApplication {
         FileSystemResource file = new FileSystemResource(filePath);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", new String(file.getFilename().getBytes("utf-8"), "ISO8859-1")));
+        headers.add("Content-Disposition", String.format("attachment;filename=%s", new String(file.getFilename().getBytes("utf-8"), "ISO8859-1")));
         headers.add("Pragma", "no-cache");
         headers.add("Expires", "0");
 
